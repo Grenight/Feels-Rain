d3e636774a8aa19c208e90eb4c343be97b835979
Grenight <true.grenight@gmail.com>
2019-02-21 14:05:19 +0200

    Now rain drops reset before touch bottom of screen

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import time
 
 # Setup
 pygame.init()
+fps = 60
 wight = 480
 height = 640
 screen = pygame.display.set_mode((wight, height))
@@ -14,44 +15,63 @@ background = pygame.Surface(screen.get_size())
 # Rain Drops
 class Drop(object):
 
-    __slots__ = ['x', 'y', 'y_speed', 'gravity']
+    __slots__ = ['x', 'y', 'y_speed', 'gravity', 'surface']
 
-    def __init__(self, x, y, y_speed, gravity):
-        self.x = x
-        self.y = y
-        self.y_speed = y_speed
-        self.gravity = gravity
+    def __init__(self, x, y, y_speed,gravity, surface):
+        self.x: float = x
+        self.y: float = y
+        self.y_speed: float = y_speed
+        self.gravity: float = gravity
+        self.surface: int  = surface
+
+    def reset(self):
+        self.y = 0
+        self.x = random.randint(0, wight)
+        self.y_speed = random.randint(3, 10)
+        self.surface = random.randint(height - height / 5, height)
 
     def fall(self):
         self.y += self.y_speed
         self.y_speed += self.gravity / 10
         if self.y > height:
-            self.y = random.randint(-100, 0)
-            self.x = random.randint(0, wight)
-            self.y_speed = random.randint(3, 10)
+            self.reset()
 
-    def show(self):
-        pygame.draw.line(background,
-                         (138, 43, 226),
-                         (self.x, self.y),
-                         (self.x, self.y + 10))
+    def draw(self):
+        if self.y < self.surface:
+            drop.fall()
+            pygame.draw.line(background,
+                             (138, 43, 226),
+                             (self.x, self.y),
+                             (self.x, self.y + 10))
+        else:
+            self.reset()
 
+    def splash(self):
+        """Todo: add splash when drop hit """
+        pygame.draw.aaline(background,
+                            (138, 43, 226),
+                            (self.x, self.y),
+                            (self.x - random.randint(3, 10),
+                             self.y - random.randint(3, 10)))
 
 
 # Creating rain drops
 all_drops = list()
 
-for i in range(250):
+for i in range(300):
     s_x = random.randint(0, wight)
     s_y = random.randint(-200, 0)
     s_speed = random.randint(3, 10)
     s_gravity = random.randint(0, 2)
-    obj = Drop(s_x, s_y, s_speed, s_gravity)
+    surface = random.randint(height - height / 5, height)
+    obj = Drop(s_x, s_y, s_speed, s_gravity, surface)
+
     all_drops.append(obj)
 
 
 # Main Game Loop
 loop = True
+clock = pygame.time.Clock()
 while loop:
     start = time.time()
     start_w = time.time()
@@ -62,8 +82,9 @@ while loop:
     background = background.convert()
     start = time.time()
     for drop in all_drops:
-        drop.fall()
-        drop.show()
+        drop.draw()
     screen.blit(background, (0, 0))
     pygame.display.flip()
-    print(f'One frame {round(time.time() - start, 5)}')
+    print(f'One cpu frame {round(time.time() - start, 5)}')
+    clock.tick(fps)
+
