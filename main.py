e9a32641bc29fbcea3adf38ddb50541ec4bced5c
Grenight <true.grenight@gmail.com>
2019-02-24 16:32:41 +0200

    Added pepe and splash when drops hits him

--- main.py
+++ main.py
@@ -6,16 +6,16 @@ import time
 pygame.init()
 
 # FPS of game
-FPS = 60
+FPS = 30
 
 # How many rain drops there is
-NUMBER_OF_DROPS = 2800
+NUMBER_OF_DROPS = 600
 
 # Screen resolution
-WIGHT = 1280
-HEIGHT = 720
+WIGHT = 600
+HEIGHT = 600
 
-# Point list for ground
+# Point list for drawing ground
 point_list = [(0, HEIGHT - HEIGHT // 7),
               (WIGHT, HEIGHT - HEIGHT // 7),
               (WIGHT, HEIGHT),
@@ -26,30 +26,54 @@ screen = pygame.display.set_mode((WIGHT, HEIGHT))
 pygame.display.set_caption('Rain')
 background = pygame.Surface(screen.get_size())
 
-# Wind settings
-wind = 0
+# WIND settings
+WIND = 0
+WIND_MAX_SPD = 20
+
+# Mouse settings
+mouse_x, mouse_y = pygame.mouse.get_pos()
+
+# Colors
+WHITE = (255, 255, 255)
+
+# Pepe Size
+PEPE_X = 350
+PEPE_Y = 274
+
+
+# Sprite Class
+class MyDude(pygame.sprite.Sprite):
+    def __init__(self, width, height):
+        # Call parent class (Sprite) constructor
+        super().__init__()
+
+        # Pass in the color of the car, and its x and y position, width and height.
+        # Set the background color and set it to be transparent
+        self.image = pygame.Surface([width, height])
+        self.image.fill(WHITE)
+        self.image.set_colorkey(WHITE)
+        self.image = pygame.image.load("pepe.png").convert_alpha()
+
+        # Fetch the rectangle object that has the dimensions of the image.
+        self.rect = self.image.get_rect()
+        self.mask = pygame.mask.from_surface(self.image)
 
 
 # Rain Drops class
 class Drop(object):
 
-    __slots__ = ['x', 'y', 'y_speed', 'gravity', 'surface', 'thickness', 'splash_frames']
-
-    def __init__(self,
-                 x, y,
-                 y_speed,
-                 gravity,
-                 surface,
-                 thickness,
-                 splash_frames):
-
-        self.x: float = x
-        self.y: float = y
-        self.y_speed: float = y_speed
-        self.gravity: float = gravity
-        self.surface: int = surface
-        self.thickness: int = thickness
-        self.splash_frames: int = splash_frames
+    def __init__(self,):
+        # Initialization
+        self.x: float = random.randint(0, WIGHT)
+        self.y: float = random.randint(-HEIGHT, 0)
+        self.y_speed: float = random.randint(3, 5)
+        self.gravity: float = random.randint(0, 2)
+        self.surface: int = (random.randint
+                             (HEIGHT - HEIGHT // 7,
+                              HEIGHT - HEIGHT // 20))
+        self.thickness: int = random.randint(1, 2)
+        self.splash_frames: int = 0
+        self.is_splash = True
 
     def reset(self):
         # Moving Drop to top of screen and randomizing starting parameters
@@ -60,12 +84,13 @@ class Drop(object):
         self.gravity: float = random.randint(0, 2)
         self.thickness: int = random.randint(1, 2)
         self.splash_frames: int = 5
+        self.is_splash = True
 
     def fall(self):
         # Moving drop down and increasing speed
         self.y += self.y_speed
         self.y_speed += self.gravity / 20
-        self.x += wind
+        self.x += WIND
         if self.x < 0:
             self.x = WIGHT
         if self.x > WIGHT:
@@ -80,17 +105,26 @@ class Drop(object):
                 background,
                 (141, 190, 214),
                 (self.x, self.y),
-                (self.x - wind, self.y - self.y_speed))
+                (self.x - WIND, self.y - self.y_speed))
         else:
             self.splash()
+        if 50 < self.x < 250:
+            if HEIGHT - 250 < self.y < HEIGHT:
+                self.splash()
 
     def mouse_interaction(self):
-        mouse_x, mouse_y = pygame.mouse.get_pos()
+        # Splash if touch cursor
         if mouse_x < self.x < mouse_x + 10:
                 if mouse_y < self.y < mouse_y + 20:
                     self.splash()
 
+    def splash_size(self):
+        if self.is_splash:
+            self.is_splash = False
+            self.splash_frames = self.y_speed // 3
+
     def splash(self):
+        self.splash_size()
         if self.splash_frames > 0:
             self.splash_frames -= 1
             for drops in range(random.randint(3, 6)):
@@ -98,7 +132,7 @@ class Drop(object):
                     background,
                     (141, 190, 214),
                     (self.x, self.y),
-                    (self.x - random.randint(-8 - wind//2, 8 - wind),
+                    (self.x - random.randint(-8 - WIND // 1.5, 8 - WIND // 1.5),
                      self.y - random.randint(0, int(self.y_speed)//2)))
         else:
             self.reset()
@@ -108,21 +142,18 @@ class Drop(object):
 all_drops = list()
 
 for i in range(NUMBER_OF_DROPS):
-    s_x = random.randint(0, WIGHT)
-    s_y = random.randint(-500, -20)
-    s_speed = random.randint(3, 5)
-    s_gravity = random.randint(0, 2)
-    s_surface = random.randint(HEIGHT - HEIGHT // 7, HEIGHT - HEIGHT // 20)
-    s_thickness = random.randint(1, 2)
-    s_splash_frames = 5
-    obj = Drop(s_x, s_y,
-               s_speed,
-               s_gravity,
-               s_surface,
-               s_thickness,
-               s_splash_frames)
+    obj = Drop()
     all_drops.append(obj)
 
+# List for sprites
+all_sprites = pygame.sprite.Group()
+
+# Creating pepe
+pepe = MyDude(PEPE_X // 2, PEPE_Y // 2)
+pepe.rect.x = 0
+pepe.rect.y = HEIGHT - PEPE_Y
+
+all_sprites.add(pepe)
 
 # Main Game Loop
 loop = True
@@ -130,24 +161,37 @@ clock = pygame.time.Clock()
 
 while loop:
     start = time.time()
-    start_w = time.time()
+    mouse_x, mouse_y = pygame.mouse.get_pos()
+    pygame.display.update()
+
+    # Process all keyboard events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             loop = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                wind -= 1
+                if WIND > -WIND_MAX_SPD:
+                    WIND -= 2
             if event.key == pygame.K_RIGHT:
-                wind += 1
+                if WIND < WIND_MAX_SPD:
+                    WIND += 2
+
+    # Draw background and polygon
     background.fill((60, 132, 167))
     background = background.convert()
     pygame.draw.polygon(background, (106, 114, 113), point_list)
 
+    # Draw pepe
+    all_sprites.update()
+    all_sprites.draw(background)
+
+    # Process all drops
     for drop in all_drops:
         drop.draw()
 
+    # Update screen
     screen.blit(background, (0, 0))
-    pygame.display.update()
-    print(f'One cpu frame {round(time.time() - start, 5)}')
+
+    print(f'One cpu frame {round(time.time() - start, 4)}')
     clock.tick(FPS)
 
